e1dc6c7f2e4b7a3bc5c8b58dde1a6f23d3e027a1
Aaron D Borden <adborden@a14n.net>
2017-03-28 19:46:40 -0700

    Rearrange settings to be easier for development

--- ballot_api/settings/common.py
+++ ballot_api/settings/common.py
@@ -76,8 +76,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
--- ballot_api/settings/heroku.py
+++ ballot_api/settings/heroku.py
@@ -1,5 +1,13 @@
-from .common import *
+import os
+
 import dj_database_url
+
+from .common import *
 
 
 DATABASES['default'] = dj_database_url.config()
+
+SECRET_KEY = os.environ.get('SECRET_KEY', None)
+
+if not SECRET_KEY:
+    raise Exception('SECRET_KEY environment variable must be set for heroku configuration.')
