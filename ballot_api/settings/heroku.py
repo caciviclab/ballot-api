3ad1a28968062ef8cadd473538efdede03501c00
Aaron D Borden <adborden@a14n.net>
2017-04-09 18:51:07 -0700

    Configurable ALLOWED_HOSTS
    Clean up settings guards for helpful hints setting environment settings.

--- ballot_api/settings/heroku.py
+++ ballot_api/settings/heroku.py
@@ -8,13 +8,20 @@ from .common import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'caciviclab-ballot-api.herokuapp.com',
-]
+
+if 'ALLOWED_HOSTS' not in os.environ:
+    raise Exception('ALLOWED_HOSTS environment variable must be set to a comma-separated list of host names for heroku configuration.')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+
+
+if 'DATABASE_URL' not in os.environ:
+    raise Exception('DATABASE_URL environment variable must be set for heroku configuration.')
 
 DATABASES['default'] = dj_database_url.config()
 
-SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
-if not SECRET_KEY:
+if 'SECRET_KEY' not in os.environ:
     raise Exception('SECRET_KEY environment variable must be set for heroku configuration.')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
